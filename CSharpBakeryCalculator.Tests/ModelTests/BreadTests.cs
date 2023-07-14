246e99befeaa219bfb954c71e691edc5fa3827b3
Daniel Kiss <flowfast47@gmail.com>
2023-07-14 08:46:28 -0700

    add testing setup

---
+++ CSharpBakeryCalculator.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using CSharpBakeryCalculator.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpBakeryCalculator.Tests
+{
+
+}
